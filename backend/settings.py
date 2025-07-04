6cf7763eacb56d3eabd18e5011515ce3d5c48e70
kcong0039 <kcong@andrew.cmu.edu>
2025-07-04 12:54:23 -0400

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-!%j+su_8!)v$)9^-w*4=%frp5l1-x8lssa$+i48ml2czhri8+j'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["127.0.0.1",
     "localhost",
